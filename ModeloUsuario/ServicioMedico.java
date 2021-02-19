ecc7d2a5b98e2e7bd9062c7e9a9ef0dbd4cadfe9
Usuario <rafaelmonrroy045@micorreo.upp.edu.mx>
2021-02-18 19:09:13 -0600

    Rafa

---
+++ ModeloUsuario/ServicioMedico.java
@@ -0,0 +1,39 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class ServicioMedico extends Usuario {
+
+    /**
+     * Default constructor
+     */
+    public ServicioMedico() {
+    }
+
+    /**
+     * 
+     */
+    private String id_Trabajador;
+
+    /**
+     * 
+     */
+    private String Horas;
+
+    /**
+     * 
+     */
+    private void list_trab() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    private void Comentarios() {
+        // TODO implement here
+    }
+
+}
