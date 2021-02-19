4e7855e94386c402031a0550fd5bde7151c8a2c6
Antonio Medina Moreno <antoniomedinamoreno@micorreo.upp.edu.mx>
2021-02-18 19:23:03 -0600

    primer commit

---
+++ PracticaGit/src/ModeloUsuario/Mantenimiento.java
@@ -0,0 +1,31 @@
+package ModeloUsuario;
+import java.util.*;
+
+/**
+ * 
+ */
+public class Mantenimiento extends Usuario {
+
+    /**
+     * Default constructor
+     */
+    public Mantenimiento() {
+    }
+    
+    private Void NombresApAm;
+    
+    private String area;
+    
+    public void Limpiar() {
+        // TODO implement here
+    }
+    
+    public void acomodar() {
+        // TODO implement here
+    }
+    
+    public void recoger_cosas() {
+    	
+    }
+
+}
